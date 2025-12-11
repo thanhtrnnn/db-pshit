fc65b6a8984896909ddc773f1c6515f9a05a5232
thanhtrnnn <thanhbx6a@gmail.com>
2025-12-11 11:52:51 +0700

    sql1755

--- solutions/01_modification/SQL1755 - Xóa tất cả các lô hàng được thực hiện từ kho 'Rio de Janeiro' đến 'Brazil' trước '2022-02-01'..sql
+++ solutions/01_modification/SQL1755 - Xóa tất cả các lô hàng được thực hiện từ kho 'Rio de Janeiro' đến 'Brazil' trước '2022-02-01'..sql
@@ -1,6 +1,11 @@
 DELETE s
-FROM shipment AS s
-JOIN warehouse AS w ON s.warehouse_id = w.id
-WHERE w.name = 'Rio de Janeiro'
-  AND w.city = 'Brazil'
-  AND s.shipped_date < '2022-02-01';
+FROM
+    shipment AS s
+JOIN
+    warehouse AS w ON s.warehouse_id = w.id
+JOIN
+    carrier AS c ON s.carrier_id = c.id
+WHERE
+    w.city = 'Rio de Janeiro'
+    AND c.name = 'Brazil'
+    AND s.shipped_date < '2022-02-01';
